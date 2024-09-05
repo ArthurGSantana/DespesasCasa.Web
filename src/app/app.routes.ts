df05092014be3ade4e81e235f78c904137dabbdc
Arthur Santana <arthurgn.santana@gmail.com>
2024-09-05 09:17:24 -0300

    feat: Add login component for public module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PublicComponent } from './containers/public/public.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./modules/public/login/login.component')
+      }
+    ]
+  }
+];
